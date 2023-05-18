3adf1d2e3f9b8110490517585a56bd8b7d800d72
Josué Acevedo (Neomatrix) <josuecevedo@gmail.com>
2023-05-18 07:55:19 -0600

    Update run_rasberry3b.sh

--- run_rasberry3b.sh
+++ run_rasberry3b.sh
@@ -9,7 +9,7 @@ sudo qemu-system-aarch64 \
 -M raspi3b \
 -append "rw earlyprintk loglevel=8 console=ttyAMA0,115200 dwc_otg.lpm_enable=0 root=/dev/mmcblk0p2 rootdelay=1" \
 -dtb bcm2710-rpi-3-b-plus.dtb \
--hda 2020-08-20-raspios-buster-armhf-full.img \
+-hda 2023-05-03-raspios-bullseye-arm64-lite.img \
 -kernel kernel8.img \
 -m 1G \
 #-smp 4 \
